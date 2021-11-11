ce48b901fc0670f3dc40f7f11cda86e31b741305
Paul Stey <paulstey@gmail>
2021-11-11 17:07:36 -0500

    wip: enum3 is still broken

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -4,7 +4,10 @@
 // I AM NOT DONE
 
 enum Message {
-    // TODO: implement the message variant types based on their usage below
+    Quit,
+    Move(Point), 
+    Echo(String),    
+    ChangeColor((i32, i32, i32)),
 }
 
 struct Point {
