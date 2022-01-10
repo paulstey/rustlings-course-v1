166e503dea45d9ab565c5ca69719332f3e4bdfa2
Paul Stey <paulstey@gmail.com>
2022-01-09 22:22:44 -0500

    feat: solve iterators2

--- exercises/standard_library_types/iterators2.rs
+++ exercises/standard_library_types/iterators2.rs
@@ -3,7 +3,6 @@
 // can offer. Follow the steps to complete the exercise.
 // As always, there are hints if you execute `rustlings hint iterators2`!
 
-// I AM NOT DONE
 
 // Step 1.
 // Complete the `capitalize_first` function.
@@ -21,7 +20,14 @@ pub fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    vec![]
+    let word_vec = words
+        .into_iter()
+        .map(|word| {
+            capitalize_first(word)
+        })
+        .collect();
+
+    word_vec
 }
 
 // Step 3.
@@ -29,8 +35,15 @@ pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
 // Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 pub fn capitalize_words_string(words: &[&str]) -> String {
-    String::new()
+    let mut word_str = String::new();
+
+    for word in capitalize_words_vector(words).iter() {
+        word_str.push_str(word);
+    }
+
+    word_str
 }
+
 
 #[cfg(test)]
 mod tests {
